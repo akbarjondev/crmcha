24dc6c4bb7df35009818710ae30ef029470b5e77
akbarjondev <tojiyev.akbarjon@gmail.com>
2021-02-24 11:45:18 +0500

    Add products get

---
+++ src/modules/bot/products/products.js
@@ -0,0 +1,30 @@
+const { fetch } = require('./../../../db/db.js')
+
+const GET = async (req, res) => {
+	try {
+		const SELECT_ALL_PRODUCTS = `
+			select
+				p.product_name, 
+				p.product_price, 
+				p.product_image, 
+				p.product_info,
+				r.reserve_product_count
+			from reserves as r
+			join products as p on r.product_id = p.product_id;
+		`
+
+		const allProducts = await fetch(SELECT_ALL_PRODUCTS)
+		
+		res.send({
+			status: 200,
+			message: 'ok',
+			data: allProducts
+		})
+	} catch(e) {
+		console.log(e)
+
+		res.send(e)
+	}
+}
+
+module.exports.GET = GET
--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const { fetch } = require('./db/db.js')
 const { PORT } = require('./config/config.js')
 
 const BotClients = require('./modules/bot/clients/clients.js')
+const BotProducts = require('./modules/bot/products/products.js')
 
 const run = (app, express) => {
 
@@ -17,12 +18,15 @@ const run = (app, express) => {
 		res.send('Admin Panel')
 	})
 
-	// API
+	// BOT API 
 	// add client
 	app.post('/bot/clients', BotClients.POST)
 
 	// edit client
 	app.put('/bot/clients', BotClients.PUT)
+
+	// get all products ready at reserve
+	app.get('/bot/products', BotProducts.GET)
 
 	app.listen(PORT, () => console.log(`ready at http://localhost:${PORT}`))
 }
