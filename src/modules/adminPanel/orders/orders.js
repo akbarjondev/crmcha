8d58ca908cc155b791761e029c5fc27327420bd3
akbarjondev <tojiyev.akbarjon@gmail.com>
2021-02-24 22:04:41 +0500

    Add AdminPanel orders module

---
+++ src/modules/adminPanel/orders/orders.js
@@ -0,0 +1,47 @@
+const { fetch } = require('./../../../db/db.js')
+
+const GET = async (req, res) => {
+
+	try {
+		const GET_ALL_SALES = `
+			select
+			  s.sale_id,
+			  s.sale_date,
+			  s.sale_completed,
+			  s.sale_product_count,
+			  s.sale_product_count,
+			  l.latitude,
+			  l.longitude,
+			  p.product_name,
+			  p.product_price,
+			  p.product_image,
+			  p.product_info,
+			  c.tg_username,
+			  c.tg_phone,
+			  c.tg_first_name,
+			  c.tg_last_name
+			from
+			  sales as s
+			join
+			  products as p on p.product_id = s.product_id
+			join
+			  locations as l on l.location_id = s.location_id
+			join
+				clients as c on c.client_id = s.client_id
+			;
+		`
+
+		res.send({
+			status: 200,
+			message: 'ok',
+			data: await fetch(GET_ALL_SALES)
+		})
+
+	} catch(e) {
+		console.log(e)
+
+		res.send(e)
+	}
+}
+
+module.exports.GET = GET
