a9e4857bfdbc5a495c4fddc445484ffab480a4f3
akbarjondev <tojiyev.akbarjon@gmail.com>
2021-03-02 16:22:26 +0500

    Add orders modules

---
+++ src/modules/bot/orders/orders.js
@@ -0,0 +1,40 @@
+const { fetch } = require('./../../../db/db.js')
+
+const POST = async (req, res) => {
+	
+	const { sale_product_count, product_id, client_id, location_id } = req.body
+
+	try {
+		
+		const addNewOrder = await fetch(`
+			insert into sales
+				sale_product_count,
+				product_id,
+				client_id,
+				location_id
+			values
+				($1, $2, $3, $4)
+			returning
+				sale_id,
+				sale_date,
+				sale_product_count,
+				product_id,
+				client_id,
+				location_id
+		`, sale_product_count, product_id, client_id, location_id)
+
+		res.send({
+			status: 200,
+			message: 'ok',
+			data: addNewOrder
+		})
+	} catch(e) {
+		console.log(e)
+
+		res.send(e)
+	}
+}
+
+module.exports = {
+	POST,
+}
