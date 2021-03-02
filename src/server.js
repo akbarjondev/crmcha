9c62254e4abcccda6072f7bd6a2da98622181d33
akbarjondev <tojiyev.akbarjon@gmail.com>
2021-03-02 16:22:59 +0500

    Refactor server

--- src/server.js
+++ src/server.js
@@ -1,16 +1,20 @@
 const ejs = require('ejs')
+const cors = require('cors')
 const parser = require('co-body')
 const { fetch } = require('./db/db.js')
 const { PORT } = require('./config/config.js')
 
+const BotOrders = require('./modules/bot/orders/orders.js')
 const BotClients = require('./modules/bot/clients/clients.js')
 const BotProducts = require('./modules/bot/products/products.js')
 const BotLocations = require('./modules/bot/locations/locations.js')
+
 const AdminOrders = require('./modules/adminPanel/orders/orders.js')
 
 const run = (app, express) => {
 
 	app.use(express.json())
+	app.use(cors())
 
 	app.engine('html', ejs.renderFile)
 	app.set('view engine', 'html')
@@ -23,11 +27,20 @@ const run = (app, express) => {
 	// add client
 	app.post('/bot/clients', BotClients.POST)
 
+	// find client by tg_user_id
+	app.get('/bot/client/:tg_user_id', BotClients.GET_ONE)
+
 	// edit client
 	app.put('/bot/clients', BotClients.PUT)
 
 	// get all products ready at reserve
 	app.get('/bot/products', BotProducts.GET)
+
+	// get one product ready at reserve
+	app.get('/bot/product/:product_id', BotProducts.GET_ONE)
+
+	// add new order || sale
+	app.post('/bot/order', BotOrders.POST)
 
 	// get locations
 	app.get('/bot/locations', BotLocations.GET)
